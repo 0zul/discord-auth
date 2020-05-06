0a52b5cf1c1fe6c9db770cb5fa3c8322b385d800
Looney <devnilghosh2002@gmail.com>
2020-05-06 20:42:35 +0530

    Added v1.0.0

---
+++ example/index.js
@@ -0,0 +1,57 @@
+const express  = require('express');
+const session  = require('express-session');
+const passport = require('passport');
+const Strategy = require('../lib').Strategy;
+const app      = express();
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+const scopes = ['identify', 'email', 'connections', 'guilds', 'guilds.join'];
+
+passport.use(new Strategy({
+    clientID: '',
+    clientSecret: '',
+    callbackURL: 'http://localhost:5000/callback',
+    scope: scopes
+}, function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function() {
+        return done(null, profile);
+    });
+}));
+
+app.use(session({
+    secret: 'youshallnotpass',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.get('/', passport.authenticate('discord', { scope: scopes }), function(req, res) {});
+app.get('/callback',
+    passport.authenticate('discord', { failureRedirect: '/' }), function(req, res) { res.redirect('/info') } // auth success
+);
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+app.get('/info', checkAuth, function(req, res) {
+    //console.log(req.user)
+    res.json(req.user);
+});
+
+
+function checkAuth(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.send('not logged in :(');
+}
+
+
+app.listen(5000, function (err) {
+    if (err) return console.log(err)
+    console.log('Listening at http://localhost:5000/')
+})
